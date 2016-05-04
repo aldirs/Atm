6c62b3f7725e5696ee3ecf75079333203c4b790d
Luke Chiu <kenyacho@msn.com>
2016-05-04 21:58:37 +0800

    CONSTANT

--- app/src/main/java/com/luke/atm/MainActivity.java
+++ app/src/main/java/com/luke/atm/MainActivity.java
@@ -1,41 +1,76 @@
 package com.luke.atm;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
 
     private static final int FUNC_LOGIN = 6;
     boolean logon = false;
+    String[] data = {"餘額查詢", "交易明細", "最新消息", "投資理財", "離開"};
+    int[] icons = {R.drawable.icon_search,
+    R.drawable.icon_list,
+    R.drawable.icon_news,
+    R.drawable.icon_money,
+    R.drawable.icon_quit};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (!logon){
+        if (!logon) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivityForResult(intent, FUNC_LOGIN);
         }
         //startActivity(new Intent(this, TestActivity.class));
         findviews();
+
         ListView lv = (ListView) findViewById(R.id.listView);
-        String[] data = {"AAAA", "BBBB", "CCCC","DDDD"};
+
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
         lv.setAdapter(arrayAdapter);
+        //lv.setOnItemClickListener(this);
+//Spinner
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //標準用法 下兩行
+//        String[] spinArray = getResources().getStringArray(R.array.spinner_array);
+//        ArrayAdapter arrayAdapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, spinArray);
+
+        //改用類別層級 建立一個Resource 在Value的Array.xml中, 再抓出來
+        ArrayAdapter arrayAdapter2 = ArrayAdapter.createFromResource(this, R.array.spinner_array,
+                android.R.layout.simple_spinner_item);
+        //選下拉時的Layout
+        arrayAdapter2.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(arrayAdapter2);
+
+        spinner.setOnItemSelectedListener(this);
+//Spinner End
+//GridView
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+
+        IconAdapter iconAdapter = new IconAdapter();
+        gridView.setAdapter(iconAdapter);
+        gridView.setOnItemClickListener(this);
+//GridView End
 
     }
 
     private void findviews() {
-
 
     }
 
@@ -48,7 +83,7 @@ public class MainActivity extends BaseActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
-        switch (itemId){
+        switch (itemId) {
             case R.id.action_settings:
                 Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_LONG).show();
                 break;
@@ -61,9 +96,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == FUNC_LOGIN){
+        if (requestCode == FUNC_LOGIN) {
 
-            if (resultCode == RESULT_OK){
+            if (resultCode == RESULT_OK) {
                 Toast.makeText(this, R.string.Result_OK, Toast.LENGTH_LONG).show();
                 String userid = data.getStringExtra("USERID");
                 String userpwd = data.getStringExtra("USERPWD");
@@ -72,10 +107,78 @@ public class MainActivity extends BaseActivity {
                         .putString("USERID", userid)
                         .commit();
 
-                Log.d("LOGIN:", userid+","+userpwd);
-            }else {
+                Log.d("LOGIN:", userid + "," + userpwd);
+            } else {
                 finish();
             }
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String[] spinArray = getResources().getStringArray(R.array.spinner_array);
+        Log.d("Select Spinner", position+","+spinArray[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//實作多個同方法時所採用的方式
+//        int parentId = parent.getId();
+//        switch (parentId){
+//            case R.id.gridView:
+//                break;
+//            case R.id.listView:
+//                break;
+//        }
+        switch (position){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                finish();
+                break;
+        }
+    }
+    class IconAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return icons.length; //長度
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data[position];  //傳String 或 Int都可以, 目前傳String 給他
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position; //給position
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (convertView ==null){
+                view = getLayoutInflater().inflate(R.layout.icon, null);
+                TextView tv = (TextView) view.findViewById(R.id.icon_text);
+                ImageView iv = (ImageView) view.findViewById(R.id.icon_image);
+                tv.setText(data[position]);
+                iv.setImageResource(icons[position]);
+
+                convertView = view;
+            }
+            return convertView;
         }
     }
 }
